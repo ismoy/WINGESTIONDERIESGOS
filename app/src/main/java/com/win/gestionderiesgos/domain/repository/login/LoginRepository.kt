9102dc3a6b95b9e2ac3d6042da17ab3bd3ff81c3
Belizaire <ismoy.belizaire@externos-cl.cencosud.cl>
2022-10-30 14:00:04 -0300

    se agrega la condicion para ingresar segun su role

--- app/src/main/java/com/win/gestionderiesgos/domain/repository/login/LoginRepository.kt
+++ app/src/main/java/com/win/gestionderiesgos/domain/repository/login/LoginRepository.kt
@@ -4,7 +4,10 @@ import android.app.Application
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.win.gestionderiesgos.data.remote.api.RetrofitInstance
 import com.win.gestionderiesgos.data.remote.provider.AuthProvider
+import com.win.gestionderiesgos.domain.model.Users
+import retrofit2.Response
 
 class LoginRepository {
     var fAuth: FirebaseAuth? = null
@@ -17,5 +20,8 @@ class LoginRepository {
     }
     suspend fun login(email:String,password:String): Task<AuthResult> {
         return authProvider.login(email, password)
+    }
+    suspend fun getOnlyUser(uidUser:String): Response<Users> {
+        return RetrofitInstance.wingestionApi.getOnlyUser(uidUser)
     }
 }
